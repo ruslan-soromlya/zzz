76acc0c367608927473e18529e625186cbfd950c
Ruslan Soromlya <rsorom@softserveinc.com>
2022-08-25 18:54:37 +0300

    dsfsdf

--- bundles/CatBundle/src/CatBundle.php
+++ bundles/CatBundle/src/CatBundle.php
@@ -3,17 +3,19 @@ namespace ruslanchyk\CatBundle;
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 
-class CatBundle extends AbstractBundle
+class CatBundle extends Bundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        // load an XML, PHP or Yaml file
-        $container->import('../config/services.yaml');
-    }
+    // public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    // {
+    //     // load an XML, PHP or Yaml file
+    //     $container->import('../config/services.yaml');
+    // }
+
     
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
---
+++ bundles/CatBundle/src/DependencyInjection/CatExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ruslanchyk\CatBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class CatExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__, '../../config')
+        );
+
+        $loader->load('../../config/services.yaml');
+    }
+}
